a640fbe06394c5849aedc03051814b8f4d740584
nchistov <nikola-chistov@mail.ru>
2022-09-07 12:03:52 +0300

    Add more rock

--- terra_craft/Assets/Scripts/WorldGenerator.cs
+++ terra_craft/Assets/Scripts/WorldGenerator.cs
@@ -6,12 +6,14 @@ public class WorldGenerator : MonoBehaviour
 {
     public GameObject[] prefabs;
 
+    public int worldWidth = 100;
+
     private GameObject _block;
     private int height = 4;
 
     void Start()
     {
-        for (float x = -57.5f; x < 58.0f; x += 1.15f) {
+        for (float x = -((worldWidth / 2) * 1.15f); x < ((worldWidth / 2) * 1.15f) + 0.5f; x += 1.15f) {
             int j = Random.Range(0, 2);
             if (j == 0) {
                 height++;
@@ -21,12 +23,16 @@ public class WorldGenerator : MonoBehaviour
                 }
             }
 
-            _block = Instantiate(prefabs[2]) as GameObject;
-                _block.transform.position = new Vector3(x, 1.15f, 0.0f);
+            for (int i = 1; i > -20; i--) {
+                _block = Instantiate(prefabs[2]) as GameObject;
+                _block.transform.position = new Vector3(x, (1.15f * i), 0.0f);
+            }
+
             for (int i = 2; i < height + 1; i++) {
                 _block = Instantiate(prefabs[1]) as GameObject;
                 _block.transform.position = new Vector3(x, (1.15f * i), 0.0f);
             }
+
             _block = Instantiate(prefabs[0]) as GameObject;
             _block.transform.position = new Vector3(x, (1.15f * height+1) + 0.15f, 0.0f);
         }
