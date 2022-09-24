c511177bd71b9fb2a2a0dfa0ca8345c7dbc850ee
nchistov <nikola-chistov@mail.ru>
2022-09-24 10:31:02 +0300

    Add faster placing blocks

--- terra_craft/Assets/Scripts/FollowMouse.cs
+++ terra_craft/Assets/Scripts/FollowMouse.cs
@@ -8,6 +8,10 @@ public class FollowMouse : MonoBehaviour
     [SerializeField] private GameObject world;
     public WorldGenerator wg;
 
+    private Vector3 lastPos = new Vector3(1, 1, 0);
+
+    public int cubeType = 0;
+
     void Start()
     {
         wg = world.GetComponent<WorldGenerator>();
@@ -18,10 +22,15 @@ public class FollowMouse : MonoBehaviour
         Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(pos.x - (pos.x % 1.15f), pos.y - (pos.y % 1.15f), 0);
 
-        if(Input.GetMouseButtonDown(0)) {
-            if (!WorldGenerator.RemoveObject(transform.position)) {
-                wg.AddObject(transform.position, 0);
+        if(Input.GetMouseButton(0)) {
+            if (transform.position != lastPos) {
+                if (!WorldGenerator.RemoveObject(transform.position)) {
+                    wg.AddObject(transform.position, cubeType);
+                }
+                lastPos = transform.position;
             }
+        } else {
+            lastPos = new Vector3(1, 1, 0);
         }
     }
 }
