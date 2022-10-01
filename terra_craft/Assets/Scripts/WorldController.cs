0ecd8c5ff62ca8a4ce9b8435fde405998ea4102d
nchistov <nikola-chistov@mail.ru>
2022-10-01 14:09:44 +0300

    Add cubes hardness

--- terra_craft/Assets/Scripts/WorldController.cs
+++ terra_craft/Assets/Scripts/WorldController.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class WorldController : MonoBehaviour
 {
     private static Dictionary<GameObject, int> blocks = new Dictionary<GameObject, int>();
+    private static Dictionary<int, List<float>> BLOCK_DATA = new Dictionary<int, List<float>>();
 
     [SerializeField] private GameObject[] prefabs;
     [SerializeField] private GameObject cursor;
     [SerializeField] private GameObject removing;
 
     private static GameObject _block;
+    private List<float> add = new List<float>();
+
+    public float currentInstremunt = 0.0f;
 
     void Start()
     {
-        
+        // Кулак, меч, кирка, топор, лопата и мотыга.
+        //   0     1     2      3      4        5
+
+        // дерн
+        add.Add(0.1f); add.Add(0.05f); add.Add(4.0f);
+        BLOCK_DATA.Add(0, new List<float>(add));
+        add.Clear();
+
+        // земля
+        add.Add(0.1f); add.Add(0.05f); add.Add(4.0f);
+        BLOCK_DATA.Add(1, new List<float>(add));
+        add.Clear();
+
+        // камень
+        add.Add(0.2f); add.Add(0.08f); add.Add(2.0f);
+        BLOCK_DATA.Add(2, new List<float>(add));
     }
 
     public GameObject GetObject(Vector3 position)
@@ -41,14 +60,21 @@ public class WorldController : MonoBehaviour
     public IEnumerator RemoveBlock(Vector3 position)
     {
         if (ExistObject(position)) {
+            float time;
             GameObject block = GetObject(position);
             Vector3 pos1 = position;
             removing.SetActive(true);
             removing.transform.position = pos1;
+            List<float> data = BLOCK_DATA[blocks[block]];
+            if (data[2] == currentInstremunt) {
+                time = data[1];
+            } else {
+                time = data[0];
+            }
             for(int i = 0; i < 8; i++) {
                 removing.transform.localScale = new Vector3(removing.transform.localScale.x + 0.1f, removing.transform.localScale.y + 0.1f,
                                                         removing.transform.localScale.z);
-                yield return new WaitForSeconds(0.05f);
+                yield return new WaitForSeconds(time);
                 removing.SetActive(true);  // Этот код должен замениться.
 
                 if (!Input.GetMouseButton(0) || !(cursor.transform.position == pos1)) {
